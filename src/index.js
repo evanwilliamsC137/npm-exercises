86b7647ce65dc20043ac632c2e84631d14fabc3d
Evan Williams <evan.b.williams95@gmail.com>
2021-09-27 10:52:06 -0500

    adding movie api

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ const sayHello = require("./say-hello")
 // sayHello();
 
 console.log(sayHello);
+
+const {getMovies} = require('./api.js');
+
+getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
+}).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
+});
